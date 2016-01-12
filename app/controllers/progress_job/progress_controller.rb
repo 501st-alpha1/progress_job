90c16a2721202468f37411404c2fcafadc3cab48
Scott Weldon <scvdata@scott-weldon.com>
2016-01-12 11:26:43 -0800

    Fix syntax errors

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -5,10 +5,10 @@ module ProgressJob
       @delayed_job = Delayed::Job.find_by(id: params[:job_id])
       if @delayed_job
         hash = {
-          progress_max: @delayed_job.progress_max,
-          progress_current: @delayed_job.progress_current
+          :progress_max => @delayed_job.progress_max,
+          :progress_current => @delayed_job.progress_current
         }
-        hash.percentage = !hash.progress_max.zero? ? hash.progress_current / hash.progress_max.to_f * 100 : 0
+        hash[:percentage] = !hash[:progress_max].zero? ? hash[:progress_current] / hash[:progress_max].to_f * 100 : 0
         render json: hash.to_json
       else
         render json: {job_not_found: true}.to_json
