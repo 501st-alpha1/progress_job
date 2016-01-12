29915d92310de19d1e637c33904b91ec01d31f07
Scott Weldon <scvdata@scott-weldon.com>
2016-01-12 10:56:06 -0800

    Use hash of only specific delayed_job attributes

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -4,8 +4,12 @@ module ProgressJob
     def show
       @delayed_job = Delayed::Job.find_by(id: params[:job_id])
       if @delayed_job
+        hash = {
+          progress_max: @delayed_job.progress_max,
+          progress_current: @delayed_job.progress_current
+        }
         percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
-        render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
+        render json: hash.merge!(percentage: percentage).to_json
       else
         render json: {job_not_found: true}.to_json
       end
